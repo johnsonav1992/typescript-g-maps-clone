ba085829d610ecff20550ca8053b273b84f41cbf
Alex Johnson <johnsonav1992@gmail.com>
2022-11-01 06:49:26 -0500

    set up basic TS

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!
+const addressInput = document.querySelector('#address')! as HTMLInputElement
+
+const searchAddressHandler = (event: Event) => {
+	event.preventDefault()
+  const enteredAddress = addressInput.value
+
+  //send to Google's API
+}
+
+form.addEventListener('submit', searchAddressHandler)
