2f8e7f2557364e4e8bf8c929938fc2295acdedf3
Alex Johnson <johnsonav1992@gmail.com>
2022-11-01 07:19:08 -0500

    map functionality added!

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,46 @@
+import axios from 'axios'
+
 const form = document.querySelector('form')!
 const addressInput = document.querySelector('#address')! as HTMLInputElement
 
+const GOOGLE_API_KEY = 'AIzaSyAJH9BwhKTRkugTS2Fc3X24Mugo-SEAkEQ'
+
+declare var google: any
+
+type GoogleGeocodingResponse = {
+	results: { geometry: { location: { lat: number; lng: number } } }[]
+	status: 'OK' | 'ZERO_RESULTS'
+}
+
 const searchAddressHandler = (event: Event) => {
 	event.preventDefault()
-  const enteredAddress = addressInput.value
+	const enteredAddress = addressInput.value
 
-  //send to Google's API
+	axios
+		.get<GoogleGeocodingResponse>(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+				enteredAddress
+			)}&key=${GOOGLE_API_KEY}
+  `
+		)
+		.then(res => {
+			if (res.data.status !== 'OK') {
+				throw new Error('Could not fetch location!')
+			}
+			const coordinates = res.data.results[0].geometry.location
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+		})
+		.catch(err => {
+			console.log(err)
+			alert(err.message)
+		})
 }
 
 form.addEventListener('submit', searchAddressHandler)
